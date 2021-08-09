ab1f04611d07fcc61c10a5d49ab4f429aa2e7adf
Silvano Santana <jnkppl@hotmail.com>
2021-08-09 03:50:33 -0300

    refactor: Refactor code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import router from "@/modules/core/router";
+
+import App from "./App.vue";
+
+import "@/modules/core/assets/css/tailwind.css";
+
+createApp(App).use(router).mount("#app");
---
+++ src/modules/core/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+import authRoutes from "@/modules/auth/routes";
+import { userAuthenticated } from "@/modules/auth/state/auth";
+
+const routes: Array<RouteRecordRaw> = [...authRoutes];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (userAuthenticated.value) {
+      return next();
+    }
+
+    return next({ name: "SignIn" });
+  }
+
+  if (to.matched.some((record) => record.name === "SignIn")) {
+    if (userAuthenticated.value) {
+      return next({ name: "AindaNaoExisto" });
+    }
+  }
+
+  return next();
+});
+
+export default router;
