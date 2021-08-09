20d5f349b18349924d13e6d09bc948039c964bfe
Silvano Santana <jnkppl@hotmail.com>
2021-08-09 03:53:10 -0300

    feat: Added Service

---
+++ src/modules/auth/services/index.ts
@@ -0,0 +1,54 @@
+import { api } from "@/modules/core/api";
+import { IAuth, IForgotPassword } from "./types";
+
+export const getAuthenticatedUser = async (): Promise<unknown> => {
+  const { data } = await api.get<unknown>("/auth/me");
+
+  return data;
+};
+
+export const login = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}): Promise<IAuth> => {
+  const response = await api.post("/auth/login", { email, password });
+
+  return response.data.data;
+};
+
+export const forgotPassword = async ({
+  email,
+}: {
+  email: string;
+}): Promise<IForgotPassword> => {
+  const response = await api.post("/auth/password/email", {
+    email,
+    url: "http://localhost:8080/recuperar-senha/{token}",
+  });
+
+  return response.data.data;
+};
+
+export const resetPassword = async ({
+  email,
+  password,
+  passwordConfirmation,
+  token,
+}: {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+  token: string | string[];
+}): Promise<IForgotPassword> => {
+  const response = await api.post("/auth/password/reset", {
+    email,
+    password,
+    password_confirmation: passwordConfirmation,
+    token,
+  });
+
+  return response.data.data;
+};
---
+++ src/modules/auth/services/types.ts
@@ -0,0 +1,7 @@
+export interface IAuth {
+  access_token: string;
+}
+
+export interface IForgotPassword {
+  message: string;
+}
